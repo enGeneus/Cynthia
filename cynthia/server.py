4101d7b128d7b03413600ff8ceee327be76c7a1a
Gianluca Filippone <filgianluca93@gmail.com>
2017-05-22 13:12:54 +0200

    Added bootstrap base for browser GUI

--- cynthia/server.py
+++ cynthia/server.py
@@ -1,38 +1,69 @@
 # !/usr/bin/env python
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import curdir, sep
 import webbrowser
 
-#Server settings
+# Server settings
 PORT = 8080
-HOST = '127.0.0.1'
+HOST = 'localhost'
 
 
-class CynthiaBaseServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Send response status code
-        self.send_response(200)
+class CynthiaRequestHandler(BaseHTTPRequestHandler):
 
-        # Send headers
+    def write_error(self, code, message):
+        self.send_response(code)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-
-        # Send message back to client
-        message = "Welcome to Cynthia!"
-        # Write content as utf-8 data
         self.wfile.write(bytes(message, "utf8"))
+
+    def write_output_from_file(self, mime_type, file_path):
+        try:
+            f = open(curdir + sep + "../html_files/" + file_path)
+            self.send_response(200)
+            self.send_header('Content-type', mime_type)
+            self.end_headers()
+            self.wfile.write(bytes(f.read(), "utf8"))
+            f.close()
+        except FileNotFoundError:
+            self.write_error(404, "Not found")
+
+    def do_GET(self):
+        if self.path == "/":
+            path = "/cynthia.html"
+        else:
+            path = self.path
+
+        mime_type = 'text/html'
+        if path.endswith(".jpg"):
+            mime_type = 'image/jpg'
+        if path.endswith(".gif"):
+            mime_type = 'image/gif'
+        if path.endswith(".js"):
+            mime_type = 'application/javascript'
+        if path.endswith(".css"):
+            mime_type = 'text/css'
+        if path.endswith(".ico"):
+            mime_type = 'image/x-icon'
+
+        self.write_output_from_file(mime_type, path)
+
         return
 
 
 def run():
-    print('Starting Cynthia server...')
+    try:
+        print('Starting Cynthia server...')
 
-    server_address = (HOST, PORT)
-    httpd = HTTPServer(server_address, CynthiaBaseServer)
-    print(u'Cynthia server online, visit {host:s}:{port:d} from your browser to use the tool'
-          .format(**{'host': HOST, 'port': PORT}))
-    webbrowser.open('{host:s}:{port:d}'.format(**{'host': HOST, 'port': PORT}), new=2)
-    httpd.serve_forever()
+        server_address = (HOST, PORT)
+        server = HTTPServer(server_address, CynthiaRequestHandler)
+        print('Cynthia server online, visit {host:s}:{port:d} from your browser to use the tool'
+              .format(**{'host': HOST, 'port': PORT}))
+        webbrowser.open('{host:s}:{port:d}'.format(**{'host': HOST, 'port': PORT}), new=2)
+        server.serve_forever()
 
+    except KeyboardInterrupt:
+        print('Keyboard interrupt received, stopping Cynthia server')
+        server.socket.close()
 
 run()
