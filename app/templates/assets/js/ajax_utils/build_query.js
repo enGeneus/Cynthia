f6a44c7f40dbb867aa2976e67952f6c806a4ae88
Gianluca Filippone <filgianluca93@gmail.com>
2017-06-02 20:30:47 +0200

    Completed query building base structure

--- app/templates/assets/js/ajax_utils/build_query.js
+++ app/templates/assets/js/ajax_utils/build_query.js
@@ -1,9 +1,34 @@
+var propertyFilterHtml;
+var relationFilterHtml;
+var selectedNode_properties;
+var filtersAdded = 0;
+var relationsAdded = 0;
+
 $(function(){
 
-    var selectedNode_properties;
-    var filtersAdded = 0;
+    $.ajax("/ajax/property_form", {
+        method: "GET",
+        dataType: "html",
+        success: function(response) {
+                propertyFilterHtml = response;
+            },
+            error: function(xhr) {
+                alert("Error " + xhr.status);
+            }
+    });
 
-    $("#nodeTypeSel").change(function() {
+    $.ajax("/ajax/relations_form", {
+        method: "GET",
+        dataType: "html",
+        success: function(response) {
+                relationFilterHtml = response;
+            },
+            error: function(xhr) {
+                alert("Error " + xhr.status);
+            }
+    });
+
+    $("#startingNode").change(function() {
         var url="/ajax/node_properties";
         var selectedValue = $(this).val();
 
@@ -15,6 +40,7 @@ $(function(){
             dataType: "json",
             success: function(response) {
                 selectedNode_properties = response;
+                console.log(response);
                 fillSelectOptions($(".nodePropertySel"), selectedNode_properties);
                 removeSelectDisabled();
             },
@@ -24,33 +50,97 @@ $(function(){
         });
     });
 
-    $(".add-property-filter").click(function(e) {
+    $("#add-property-filter").click(function(e) {
         e.preventDefault();
         filtersAdded++;
-        newForm=$("<div class=\"form-group\"><label>Property:</label><select class=\"form-control nodePropertySel\"></select></div><div class=\"form-group\"><label>Input String:</label><input type=\"text\" class=\"form-control\"/>");
-        newSelect=newForm.find("select").first().attr("name", "filterName" + filtersAdded);
-        newInput=newForm.find("input").first().attr("name", "filterValue" + filtersAdded);
+        var newFilterFields=$(propertyFilterHtml);
+        newSelect=newFilterFields.find("select").first().attr("name", "filterName" + filtersAdded);
+        newInput=newFilterFields.find("input").first().attr("name", "filterValue" + filtersAdded);
         if (selectedNode_properties) {
             fillSelectOptions(newSelect, selectedNode_properties);
         } else {
             newSelect.attr("disabled", true);
         }
-        $("#query-fields").append(newForm);
+        $("#property-filters").append(newFilterFields);
+        if (filtersAdded == 1) {
+            $("#property-filters").parent().show();
+        }
+    });
+
+    $("#add-relation-filter").click(function(e) {
+        e.preventDefault();
+        relationsAdded++;
+        var newRelationFields=$(relationFilterHtml);
+        newSelect=newRelationFields.find("select").first().attr("name", "relationName" + relationsAdded);
+        newInput=newRelationFields.find("input").first().attr("name", "cutoffValue" + relationsAdded);
+        $("#relation-filters").append(newRelationFields);
+        if (relationsAdded == 1) {
+            $("#relation-filters").parent().show();
+        }
     });
 
     $("#submit-query").click(function(e) {
         e.preventDefault();
-        formJSON = JSON.stringify($("#query-form").serializeArray());
+        formJSON = serializeFormToJSON();
         console.log(formJSON);
     });
 });
 
 function fillSelectOptions(select, options) {
     for (var i in options) {
-        select.append("<option>" + options[i] + "</option>")
+        select.append("<option>" + options[i] + "</option>");
     }
 }
 
 function removeSelectDisabled() {
     $(".nodePropertySel").removeAttr("disabled");
+}
+
+function removeFilter(e, button, filterType) {
+    e.preventDefault();
+    $(button).parent().parent().remove();
+    if (filterType == 'property') {
+        filtersAdded--;
+        if (filtersAdded == 0) {
+            $("#property-filters-panel").hide();
+        }
+    } else if (filterType == 'relation') {
+        relationsAdded--;
+        if (relationsAdded == 0) {
+            $("#relation-filters-panel").hide();
+        }
+    }
+}
+
+function serializeFormToJSON() {
+    var startingNode = $("#startingNode").val();
+    var json = "{\"startingNodeType\": \"" + startingNode + "\", \"filters\": [" ;
+
+    // Add json array for property filter
+    $(".property-filter-field").each(function (index, object) {
+        var propertyName = $(object).children().first().find("select").val();
+        var propertyValue = $(object).children().last().find("input").val();
+        json = json + "{\"propertyName\": \"" + propertyName + "\", \"propertyValue\": \"" + propertyValue + "\"}, ";
+    });
+
+    // Remove last comma
+    if (filtersAdded > 0) {
+        json = json.slice(0, -2);
+    }
+
+    // Close previous array and add new one for relations
+    json = json + "], \"relationships\": [";
+    $(".relation-filter-field").each(function (index, object) {
+        var relationName = $(object).children().first().find("select").val();
+        var cutoffValue = $(object).children().last().find("input").val();
+        json = json + "{\"relationName\": \"" + relationName + "\", \"cutoffValue\": \"" + cutoffValue + "\"}, ";
+    });
+
+    // Remove last comma
+    if (relationsAdded > 0) {
+        json = json.slice(0, -2);
+    }
+
+    json = json + "]}";
+    return json;
 }
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@
 from flask import render_template, send_from_directory, request, jsonify
 from app import app
 from app import logic
+import neo4j
 
 
 @app.route('/assets/<path:path>')
@@ -20,9 +21,11 @@ def index_page():
 def query_page():
     try:
         node_labels = logic.get_node_labels()
-    except Exception:
+        message = {}
+    except neo4j.exceptions.ServiceUnavailable:
         node_labels = []
-    return render_template("build_query.html", node_labels=node_labels)
+        message = {'type': 'error', 'message': 'NO_CONN'}
+    return render_template("build_query.html", node_labels=node_labels, message=message)
 
 
 @app.route('/results', methods=['POST'])
@@ -41,3 +44,14 @@ def error_page():
 def get_node_keys():
     keys = logic.get_node_properties_keys(request.args.get('nodeType'))
     return jsonify(keys)
+
+
+@app.route('/ajax/relations_form')
+def get_node_relations():
+    relations = logic.get_name_of_relations_on_relation_general_info()
+    return render_template("form_elements/relations_filter.html", relations=relations)
+
+
+@app.route('/ajax/property_form')
+def send_form_element():
+    return render_template("form_elements/property_filter.html")
