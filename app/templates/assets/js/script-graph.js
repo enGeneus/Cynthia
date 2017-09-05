45416517d4c3ed0d5e964b971d3fda2130598b2d
lollix91 <lollix@live.it>
2017-09-05 18:49:55 +0200

    Changed d3js with cytho

--- app/templates/assets/js/script-graph.js
+++ app/templates/assets/js/script-graph.js
@@ -1,140 +1,86 @@
+var cy = cytoscape({
+  container: document.querySelector('#cy'),
 
-var margin = {top: 20, right: 120, bottom: 20, left: 120},
-    width = 960 - margin.right - margin.left,
-    height = 800 - margin.top - margin.bottom;
+  boxSelectionEnabled: false,
+  autounselectify: true,
 
-var i = 0,
-    duration = 750,
-    root;
 
-var tree = d3.layout.tree()
-    .size([height, width]);
 
-var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.y, d.x]; });
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.right + margin.left)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  style: cytoscape.stylesheet()
+    .selector('node')
+      .css({
+        'width': 'mapData(weight, 40, 80, 20, 60)',      
+        'shape': 'data(faveShape)',
+        'content': 'data(name)',
+        'text-valign': 'center',
+        'color': 'white',
+        'background-color': 'data(faveColor)',        
+        'text-outline-width': 2,
+        'text-outline-color': '#000'
+      })
+    .selector('edge')
+      .css({
+        'curve-style': 'bezier',
+        'target-arrow-shape': 'triangle',
+        'target-arrow-color': '#900',
+        'line-color': '#900',
+        'width': 1,
+        'label': 'data(label)'
+      })
+    .selector(':selected')
+      .css({
+        'background-color': 'black',
+        'line-color': 'black',
+        'target-arrow-color': 'black',
+        'source-arrow-color': 'black'
+      })
+    .selector('edge.autorotate')
+      .css({
+        'edge-text-rotation': 'autorotate'
+      })      
+    .selector('.faded')
+      .css({
+        'opacity': 0.25,
+        'text-opacity': 0
+      }),
 
-d3.json("flare.json", function(error, flare) {
-  if (error) throw error;
+  elements: {
+    nodes: [
+      { data: { id: 'j', name: 'Jerrye', weight: 65, faveColor: '#001122', faveShape: 'rectangle' } },
+      { data: { id: 'e', name: 'Elaine', weight: 65, faveColor: '#6FB1FC', faveShape: 'triangle' } },
+      { data: { id: 'k', name: 'Kramer', weight: 65, faveColor: '#6FB1FC', faveShape: 'triangle' } },
+      { data: { id: 'g', name: 'George', weight: 65, faveColor: '#6FB1FC', faveShape: 'triangle' } }
+    ],
+    edges: [
+      { data: { source: 'j', target: 'e' } },
+      { data: { source: 'j', target: 'k' } },
+      { data: { source: 'j', target: 'g' } },
+      { data: { source: 'e', target: 'j' } },
+      { data: { source: 'e', target: 'k' } },
+      { data: { source: 'k', target: 'j' } },
+      { data: { source: 'k', target: 'e' } },
+      { data: { source: 'k', target: 'g' } },
+      { data: { source: 'g', target: 'j', label: 'autorotate (move my nodes)' }, classes: 'autorotate' }
+    ]
+  },
 
-  root = flare;
-  root.x0 = height / 2;
-  root.y0 = 0;
-
-  function collapse(d) {
-    if (d.children) {
-      d._children = d.children;
-      d._children.forEach(collapse);
-      d.children = null;
-    }
+  layout: {
+    name: 'grid',
+    padding: 10
   }
-
-  root.children.forEach(collapse);
-  update(root);
 });
 
-d3.select(self.frameElement).style("height", "800px");
+cy.on('tap', 'node', function(e){
+  var node = e.cyTarget;
+  var neighborhood = node.neighborhood().add(node);
 
-function update(source) {
+  cy.elements().addClass('faded');
+  neighborhood.removeClass('faded');
+});
 
-  // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse(),
-      links = tree.links(nodes);
-
-  // Normalize for fixed-depth.
-  nodes.forEach(function(d) { d.y = d.depth * 180; });
-
-  // Update the nodes…
-  var node = svg.selectAll("g.node")
-      .data(nodes, function(d) { return d.id || (d.id = ++i); });
-
-  // Enter any new nodes at the parent's previous position.
-  var nodeEnter = node.enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
-      .on("click", click);
-
-  nodeEnter.append("circle")
-      .attr("r", 1e-6)
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-  nodeEnter.append("text")
-      .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
-      .text(function(d) { return d.name; })
-      .style("fill-opacity", 1e-6);
-
-  // Transition nodes to their new position.
-  var nodeUpdate = node.transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-  nodeUpdate.select("circle")
-      .attr("r", 4.5)
-      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
-
-  nodeUpdate.select("text")
-      .style("fill-opacity", 1);
-
-  // Transition exiting nodes to the parent's new position.
-  var nodeExit = node.exit().transition()
-      .duration(duration)
-      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
-      .remove();
-
-  nodeExit.select("circle")
-      .attr("r", 1e-6);
-
-  nodeExit.select("text")
-      .style("fill-opacity", 1e-6);
-
-  // Update the links…
-  var link = svg.selectAll("path.link")
-      .data(links, function(d) { return d.target.id; });
-
-  // Enter any new links at the parent's previous position.
-  link.enter().insert("path", "g")
-      .attr("class", "link")
-      .attr("d", function(d) {
-        var o = {x: source.x0, y: source.y0};
-        return diagonal({source: o, target: o});
-      });
-
-  // Transition links to their new position.
-  link.transition()
-      .duration(duration)
-      .attr("d", diagonal);
-
-  // Transition exiting nodes to the parent's new position.
-  link.exit().transition()
-      .duration(duration)
-      .attr("d", function(d) {
-        var o = {x: source.x, y: source.y};
-        return diagonal({source: o, target: o});
-      })
-      .remove();
-
-  // Stash the old positions for transition.
-  nodes.forEach(function(d) {
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
-}
-
-// Toggle children on click.
-function click(d) {
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
+cy.on('tap', function(e){
+  if( e.cyTarget === cy ){
+    cy.elements().removeClass('faded');
   }
-  update(d);
-}
+});
