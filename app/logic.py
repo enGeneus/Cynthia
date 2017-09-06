1876346806c4fa59ea54e8ffb06b08ccb40ec01e
jerryildrago <matteo.capodicasa@gmail.com>
2017-09-06 12:40:54 +0200

    implemented from json to query

--- app/logic.py
+++ app/logic.py
@@ -2,6 +2,7 @@
 
 from neo4j.v1 import GraphDatabase, basic_auth
 import config
+import json
 
 
 def query_db(query):
@@ -72,4 +73,90 @@ def get_species():
 
 # TO DO: Complete this method building the query, executing it and sending back the results
 def build_and_query(form_data):
-    return form_data
+    # j = json.loads(
+    # '{"startingNodeType": "microRNA", "specie": "human", "relations": [{"relationName": "miRTarBase", "cutoffValue": ""},{"relationName": "RNA22v2", "cutoffValue": ""},{"relationName": "PicTar7", "cutoffValue": ""},{"relationName": "PicTar13", "cutoffValue": ""},{"relationName": "TargetScan", "cutoffValue": ""}], "properties": [{"propertyName": "accession", "values": ["asfasf","asfdasda","danda","asdad","42342"]},{"propertyName": "name", "values": ["weRWERFAGFAS","efadsfsadf","FASDFSADF","Sdfasdfasf","fFsdfs"]},{"propertyName": "species", "values": ["DSFA","ASDFASDF","ASDFASDF"]},{"propertyName": "mirbase_link", "values": ["sdfasdfasdf","asdfasdfas","asfasdfsda"]}]}')
+
+    j = json.loads(form_data)
+
+    # numero di relazioni
+    rel_num = 0
+    # numero di proprietà
+    prop_num = 0
+
+    # controlla se ci sono le proprietà
+    if 'properties' in j:
+        prop_num = (len(j['properties']))
+
+    # parte statica della query
+    relation_part = "MATCH (n:" + j['startingNodeType'] + ")"
+
+    # controlla se ci sono le relazioni
+    if 'relations' in j:
+        rel_num = (len(j['relations']))
+        relation_part = relation_part + "-[r:"
+
+    # aggiunta delle relazioni alla query
+    for i in range(rel_num):
+
+        if rel_num == 1:
+            relation_part = relation_part + j['relations'][i]['relationName'] + "]->"
+
+        if rel_num > 1:
+            relation_part = relation_part + j['relations'][i]['relationName'] + "|"
+            if i == rel_num - 1:
+                relation_part = relation_part[:-1]
+
+    if rel_num > 1:
+        relation_part = relation_part + "]->"
+
+    # dizionario per le property, indica per ogni proprietà (chiave) quanti valori possiede (valore)
+    diz_prop = {}
+
+    relation_part = relation_part + " (t)"
+
+    if rel_num == 0:
+        relation_part = relation_part[:-3]
+
+    if rel_num > 1:
+        relation_part = relation_part + " WHERE ("
+    else:
+        relation_part = relation_part + " WHERE "
+
+    if prop_num == 0:
+        relation_part = relation_part[:-5]
+
+    # aggiornamento del dizionario
+    for i in range(prop_num):
+        prop = (len(j['properties'][i]['values']))
+        upd = {i: prop}
+        diz_prop.update(upd)
+
+    where_part = ""
+
+    # aggiunta delle proprietà alla query
+    for i in range(len(diz_prop)):
+
+        for k in range(int(diz_prop[i])):
+
+            if diz_prop[i] == 1:
+                where_part = where_part + "n." + j['properties'][i]['propertyName'] + " = " + "'" + \
+                             j['properties'][i]['values'][k] + "'"
+
+            else:
+                where_part = where_part + "n." + j['properties'][i]['propertyName'] + " = " + "'" + \
+                             j['properties'][i]['values'][k] + "'" + " OR "
+
+        if int(diz_prop[i]) > 1:
+            where_part = where_part[:-4]
+
+        if len(diz_prop) > 1 and i != len(diz_prop) - 1:
+            where_part = where_part + ") AND ("
+
+    if rel_num > 1:
+        where_part = where_part + ") "
+
+    return_part = "RETURN r,n,t"
+
+    query = relation_part + where_part + return_part
+
+    return query
