abd21e05103f1e088611ac7152788a116ed36abb
Gianluca Filippone <filgianluca93@gmail.com>
2017-09-08 00:27:40 +0200

    Refactoring of result page

--- app/templates/assets/js/ajax_utils/build_query.js
+++ app/templates/assets/js/ajax_utils/build_query.js
@@ -1,14 +1,44 @@
 
 var propertyFieldHtml = "<br/><input type=\"text\" name=\"propertyValue\" placeholder=\"value\"/><a class=\"btn btn-danger btn-xs\" style=\"padding: 5px 10px;\" onclick=\"javascript:removePropertyField(this)\"><i class=\"fa fa-times\"></i></a>";
 
+var options = []
+var startingNodes = 0;
+
 $(function(){
 
+    addStartingNode();
+
+    $("#submit-query").click(function(e) {
+        e.preventDefault();
+
+        if (startingNodes > 0) {
+            formJSON = serializeFormToJSON();
+            $("#serializedForm input").val(formJSON);
+            $("#serializedForm").submit();
+        } else {
+            alert("Error: you have to select at least one starting node");
+        }
+    });
+
+});
+
+function addStartingNode() {
+    html_to_add = $("#starting-node_pattern").clone();
+    html_to_add.removeAttr("id"); // Avoid duplicate IDs
+    $("#starting-node-container").append(html_to_add);
+
+    startingNodes++;
+    if (startingNodes > 1) {
+        $(".remove-node-button").removeClass("disabled");
+    }
+}
+
+function fillPropertyOptions(select) {
+    var url="/ajax/node_properties";
+    var selectedValue = $(select).val();
     var selectedNode_properties;
 
-    $("#startingNode").change(function() {
-        var url="/ajax/node_properties";
-        var selectedValue = $(this).val();
-
+    if (!(selectedValue in options)) {
         // Load properties
         $.ajax(url, {
             method: "GET",
@@ -17,36 +47,21 @@ $(function(){
             },
             dataType: "json",
             success: function(response) {
-                selectedNode_properties = response;
-                fillSelectOptions($("#filter_pattern .property-select"), selectedNode_properties);
+                options[selectedValue] = response;
             },
             error: function(xhr) {
                 alert("Error " + xhr.status);
             }
         });
+    }
 
-        // Enable add property button and query button
-        $(".buttons button").removeClass("disabled");
-    });
-
-    $("#add-property-filter").click(function(e) {
-        e.preventDefault();
-        html_to_add = $("#filter_pattern").clone();
-        html_to_add.removeAttr("id"); // Avoid duplicate IDs
-        $("#properties-filters").append(html_to_add);
-        html_to_add.removeAttr("hidden");
-    });
-
-    $("#submit-query").click(function(e) {
-        e.preventDefault();
-        formJSON = serializeFormToJSON();
-        console.log(formJSON);
-
-        // Serialized data are set to the form and sent to the server
-        $("#serializedForm input").val(formJSON);
-        $("#serializedForm").submit();
-    });
-});
+    // Enable add property button
+    $(select).parent().parent().find("button").removeClass("disabled");
+    // Query button
+    $("#submit-query").removeClass("disabled");
+    // Enable remove starting node button
+    $(select).parent().parent().parent().find("a").removeClass("disabled");
+}
 
 function fillSelectOptions(select, options) {
     select.empty();
@@ -64,6 +79,24 @@ function removeFilter(button) {
     $(button).closest(".property-filter").remove();
 }
 
+function removeStartingNode(button) {
+    if (startingNodes > 1) {
+        $(button).closest(".starting-node-panel").remove();
+        startingNodes--;
+        if (startingNodes == 1) {
+            $(".remove-node-button").removeAttr("disabled");
+        }
+    }
+}
+
+function addPropertyFilter(button) {
+    html_to_add = $("#filter_pattern").clone();
+    html_to_add.removeAttr("id"); // Avoid duplicate IDs
+    node_type = $(button).parent().parent().find(".starting-node-select").val();
+    fillSelectOptions(html_to_add.find("select"), options[node_type]);
+    $(button).parent().parent().find(".properties-filters").append(html_to_add);
+}
+
 function addPropertyField(button) {
     $(button).parent().children().first().append(propertyFieldHtml);
 }
@@ -75,10 +108,48 @@ function removePropertyField(button) {
 }
 
 function serializeFormToJSON() {
-    var startingNode = $("#startingNode").val();
-
-    // Add starting node type
-    var json = "{\"startingNodeType\": \"" + startingNode + "\"," ;
+    // Add starting nodes
+    var json = "{\"startingNodes\": [";
+    var nodes_added = false;
+    $(".starting-node-panel").each(function (index, object) {
+        var nodeType = $(object).find(".starting-node-select").val();
+        if (nodeType != null) {
+            nodes_added = true;
+            json = json + "{\"type\": \"" + nodeType + "\", \"properties\": [";
+            var property_added = false;
+            $(object).find(".property-filter").each(function (index, object) {
+                var propertyName = $(object).find(".property-select").val();
+                if (propertyName != null) {
+                    property_added = true;
+                    json = json + "{\"name\": \"" + propertyName + "\", \"values\": [";
+                    var value_added = false;
+                    $(object).find(".property-values input").each(function (index, object) {
+                        if ($(object).val() != "") {
+                            json = json + "\"" + $(object).val() + "\",";
+                            value_added = true;
+                        }
+                    });
+                    // Remove last comma
+                    if (value_added) {
+                        json = json.slice(0, -1);
+                    }
+                    // Close values array and property json object
+                    json = json + "]},"
+                }
+            });
+            // Remove last comma
+            if (property_added) {
+                json = json.slice(0, -1);
+            }
+            // Close properties array and starting node object
+            json = json + "]},";
+        }
+    });
+    // Remove last comma and close starting nodes array
+    if (nodes_added) {
+        json = json.slice(0, -1);
+    }
+    json = json + "],"
 
     // Add species
     $("#species-selection input").each(function (index, object) {
@@ -97,41 +168,14 @@ function serializeFormToJSON() {
             json = json + " \"cutoffValue\": \"" + $(object).find(".cutoff-value").val() + "\"},"
         }
     });
+    // Remove last comma and close relation list
     if (added_relations) {
         json = json.slice(0, -1);
     }
     json = json + "]";
 
-    // Add properties
-    json = json + ", \"properties\": [";
-    var property_added = false;
-    $(".property-filter").each(function (index, object) {
-        var propertyName = $(object).find(".property-select").val();
-        if (propertyName != null) {
-            property_added = true;
-            json = json + "{\"propertyName\": \"" + propertyName + "\", \"values\": [";
-            var value_added = false;
-            $(object).find(".property-values input").each(function (index, object) {
-                if ($(object).val() != "") {
-                    json = json + "\"" + $(object).val() + "\",";
-                    value_added = true;
-                }
-            });
-            // Remove last comma
-            if (value_added) {
-                json = json.slice(0, -1);
-            }
-            // Close values array and property json object
-            json = json + "]},"
-        }
-    });
-    // Remove last comma
-    if (property_added) {
-        json = json.slice(0, -1);
-    }
-
-    // Close property array and whole json object
-    json = json + "]}";
+    // Close json object
+    json = json + "}";
 
     return json
 }
--- app/views.py
+++ app/views.py
@@ -4,13 +4,11 @@ from flask import render_template, send_from_directory, request, jsonify
 from app import app
 from app import logic
 import neo4j
-import html
-import json
+
 
 @app.route('/assets/<path:path>')
 def send_asset(path):
     return send_from_directory('templates/assets/', path)
-
 
 
 @app.route('/')
@@ -39,58 +37,21 @@ def query_page():
                            message=message)
 
 
-
-
-@app.route('/build_query_v2')
-def query_page_v2():
-    node_labels = []
-    species = []
-    relations = []
-    try:
-        node_labels = logic.get_node_labels()
-        species = logic.get_species()
-        relations = logic.get_name_of_relations_on_relation_general_info()
-        message = {}
-    except neo4j.exceptions.ServiceUnavailable:
-        node_labels = []
-        message = {'type': 'error', 'message': 'NO_CONN'}
-    return render_template("build_query_v2.html",
-                           node_labels=node_labels,
-                           species=species,
-                           relations=relations,
-                           message=message)
-
-
 @app.route('/results', methods=['POST'])
 def query_handler():
     form_data = request.form
-    results = logic.build_and_query(form_data['json'])
-    return render_template("results.html", results=results)
+    query = logic.build_query(form_data['json'])
+    return render_template("results.html", query=query)
 
 
-@app.route('/executes_query', methods=['POST'])
+@app.route('/execute_query', methods=['POST'])
 def executes_query():
     form_data = request.form
-    results = form_data['query']
-    results=html.unescape(results)
-    #TEMP
-    #query_results=logic.query_db(results)
+    query = form_data['query']
+    #query_results=logic.query_db(query)
     query_results=logic.query_db("match(n:microRNA {name:'mmu-let-7g-3p'})-[r]->(t) RETURN n,r,t LIMIT 25")
-
-
-    #for i in query_results.data():
-    #    return render_template("empty.html", query_results=html.unescape(json.dumps(i)))
-    #while(query_results.hasNext()):
-    #toreturn=""
-    #for i in query_results.records():
-    #    toreturn=toreturn+i.values()
-    #return render_template("empty.html", query_results=html.unescape(toreturn))
-    #for r in query_results:
-    #return render_template("empty.html", query_results=(query_results.data()))
-
-
-    return render_template("empty.html", query_results=html.unescape(logic.build_json_from_query_results(query_results)))
-    #return render_template("empty.html", query_results=(query_results.keys()))
+    result_json = logic.build_json_from_query_results(query_results)
+    return result_json
 
 
 @app.route('/error')
