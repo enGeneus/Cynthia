c05479dfc69913ad7eecfb9b21abd83954531d6b
Gianluca Filippone <filgianluca93@gmail.com>
2017-09-24 19:21:59 +0200

    Added CSV export

--- app/templates/assets/js/owner/results_export.js
+++ app/templates/assets/js/owner/results_export.js
@@ -1,5 +1,12 @@
 $(function(){
 
+    $("#export-button").click(function(){
+        if ($("#results").val()!= "") {
+            $("#results-export").parent().removeClass("disabled");
+            $("#csv-export").parent().removeClass("disabled");
+        }
+    });
+
     if ($("#query").val() != "") {
         $("#query-export").parent().removeClass("disabled");
         $("#complete-query-export").removeClass("disabled");
@@ -32,4 +39,29 @@ $(function(){
         download("results.json", export_string);
     });
 
+    $("#csv-export").click(function(){
+        Json2CSV($("#results").val());
+    });
+
 });
+
+function Json2CSV(JSONstring) {
+    var objArray = JSON.parse(JSONstring);
+    var csv = "Node type,Name,Relation,Score,Target Node\r\n";
+    var data = objArray['data'];
+    for(var result in data) {
+        currentResult = data[result];
+        startingNode = currentResult['result_0'];
+        relation = currentResult['result_1'];
+        targetNode = currentResult['result_2'];
+        csv = csv + startingNode['label'] + "," + startingNode['properties']['name'] + ",";
+        csv = csv + relation['properties']['name'] + "," + relation['properties']['score'] + ",";
+        csv = csv + targetNode['properties']['name'] + "\r\n";
+    }
+    var a = document.createElement('a');
+    var blob = new Blob([csv], {'type':'application\/octet-stream'});
+    a.href = window.URL.createObjectURL(blob);
+    a.download = 'cynthia_export.csv';
+    a.click();
+    return true;
+}
