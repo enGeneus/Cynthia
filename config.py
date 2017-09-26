c8ae2204c778a9959a389ca8aee779e8b2437d60
Gianluca Filippone <filgianluca93@gmail.com>
2017-09-26 15:22:34 +0200

    Removed mysql db support

--- config.py
+++ config.py
@@ -5,17 +5,7 @@ CYNTHIA_HOST = 'localhost'
 CYNTHIA_PORT = 8080
 
 # Graph database configuration
-#GRAPH_DB_HOST = 'localhost'
-#GRAPH_DB_PORT = 7687
-#GRAPH_DB_USER = 'neo4j'
-#GRAPH_DB_PASS = 'password'
 GRAPH_DB_HOST = '13.81.8.17'
 GRAPH_DB_PORT = 7687
 GRAPH_DB_USER = 'neo4j'
 GRAPH_DB_PASS = 'neo4jpassword'
-# MySQL database configuration
-MYSQL_DB_HOST = 'localhost'
-MYSQL_DB_PORT = 8889
-MYSQL_DB_USER = 'root'
-MYSQL_DB_PASS = 'root'
-MYSQL_DB_NAME = 'cynthia_db'
